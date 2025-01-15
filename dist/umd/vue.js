81ad1f9969951b5ae0b6e74b113b9e86846b4541
wangcui10 <wangcui11@jd.com>
2025-01-15 12:46:56 +0800

    feat: 对象的数据劫持

--- dist/umd/vue.js
+++ dist/umd/vue.js
@@ -4,9 +4,81 @@
   (global = typeof globalThis !== 'undefined' ? globalThis : global || self, global.Vue = factory());
 })(this, (function () { 'use strict';
 
-  // Object.defineProperty 不能兼容IE8以下
+  function _classCallCheck(a, n) {
+    if (!(a instanceof n)) throw new TypeError("Cannot call a class as a function");
+  }
+  function _defineProperties(e, r) {
+    for (var t = 0; t < r.length; t++) {
+      var o = r[t];
+      o.enumerable = o.enumerable || false, o.configurable = true, "value" in o && (o.writable = true), Object.defineProperty(e, _toPropertyKey(o.key), o);
+    }
+  }
+  function _createClass(e, r, t) {
+    return _defineProperties(e.prototype, r), Object.defineProperty(e, "prototype", {
+      writable: false
+    }), e;
+  }
+  function _toPrimitive(t, r) {
+    if ("object" != typeof t || !t) return t;
+    var e = t[Symbol.toPrimitive];
+    if (undefined !== e) {
+      var i = e.call(t, r);
+      if ("object" != typeof i) return i;
+      throw new TypeError("@@toPrimitive must return a primitive value.");
+    }
+    return (String )(t);
+  }
+  function _toPropertyKey(t) {
+    var i = _toPrimitive(t, "string");
+    return "symbol" == typeof i ? i : i + "";
+  }
+  function _typeof(o) {
+    "@babel/helpers - typeof";
+
+    return _typeof = "function" == typeof Symbol && "symbol" == typeof Symbol.iterator ? function (o) {
+      return typeof o;
+    } : function (o) {
+      return o && "function" == typeof Symbol && o.constructor === Symbol && o !== Symbol.prototype ? "symbol" : typeof o;
+    }, _typeof(o);
+  }
+
+  function isObject(obj) {
+    return _typeof(obj) === 'object' && obj !== null;
+  }
+
+  var Observer = /*#__PURE__*/function () {
+    function Observer(value) {
+      _classCallCheck(this, Observer);
+      this.walk(value);
+    }
+    return _createClass(Observer, [{
+      key: "walk",
+      value: function walk(data) {
+        Object.keys(data).forEach(function (key) {
+          defineReactive(data, key, data[key]);
+        });
+      }
+    }]);
+  }();
+  function defineReactive(data, key, value) {
+    observer(value); // 递归实现深度检测
+    Object.defineProperty(data, key, {
+      get: function get() {
+        return value;
+      },
+      set: function set(newValue) {
+        observer(newValue); // 新值也是对象的时候需要再次监测
+        if (value !== newValue) {
+          value = newValue;
+        }
+      }
+    });
+  }
   function observer(data) {
-    console.log('observer', data);
+    if (!isObject(data)) {
+      return;
+    }
+    return new Observer(data);
   }
 
   function initState(vm) {
--- src/observer/index.js
+++ src/observer/index.js
@@ -1,4 +1,33 @@
 // Object.defineProperty 不能兼容IE8以下
+import { isObject } from '../utils/index'
+
+class Observer {
+  constructor(value) {
+    this.walk(value)
+  }
+  walk(data) {
+    Object.keys(data).forEach((key) => {
+      defineReactive(data, key, data[key])
+    })
+  }
+}
+function defineReactive(data, key, value) {
+  observer(value) // 递归实现深度检测
+  Object.defineProperty(data, key, {
+    get() {
+      return value
+    },
+    set(newValue) {
+      observer(newValue) // 新值也是对象的时候需要再次监测
+      if (value !== newValue) {
+        value = newValue
+      }
+    },
+  })
+}
 export function observer(data) {
-  console.log('observer', data)
+  if (!isObject(data)) {
+    return
+  }
+  return new Observer(data)
 }
